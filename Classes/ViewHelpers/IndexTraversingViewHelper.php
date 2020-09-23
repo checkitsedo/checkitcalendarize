6c50e34458641e549032aea5d9c949e682629ff5
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 19:31:41 +0200

    Update IndexTraversingViewHelper.php

--- Classes/ViewHelpers/IndexTraversingViewHelper.php
+++ Classes/ViewHelpers/IndexTraversingViewHelper.php
@@ -5,10 +5,10 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\ViewHelpers;
+namespace Checkitsedo\Checkitcalendarize\ViewHelpers;
 
-use HDNET\Calendarize\Domain\Model\Index;
-use HDNET\Calendarize\Domain\Repository\IndexRepository;
+use Checkitsedo\Checkitcalendarize\Domain\Model\Index;
+use Checkitsedo\Checkitcalendarize\Domain\Repository\IndexRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
@@ -19,7 +19,7 @@ use TYPO3\CMS\Extbase\Persistence\QueryInterface;
  * == Examples ==
  *
  * <code title="Traversing thru future and past occurings of the event">
- * {namespace c=HDNET\Calendarize\ViewHelpers}
+ * {namespace c=Checkitsedo\Checkitcalendarize\ViewHelpers}
  * <f:for each="{c:indexTraversing(index:'{index}', future: 1, past: 0, limit: 10, sort: 'ASC', useIndexTime: 1)}" as="futureEvent">
  *  <f:debug>{futureEvent}</f:debug>
  * </f:for>
