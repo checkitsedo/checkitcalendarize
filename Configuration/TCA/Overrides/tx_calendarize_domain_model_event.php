56f1ed50e0b256f8fceb61c46423790ba45c6e25
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 20:00:01 +0200

    Update tx_calendarize_domain_model_event.php

--- Configuration/TCA/Overrides/tx_calendarize_domain_model_event.php
+++ Configuration/TCA/Overrides/tx_calendarize_domain_model_event.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use HDNET\Calendarize\Register;
-use HDNET\Calendarize\Utility\ConfigurationUtility;
+use Checkitsedo\Checkitcalendarize\Register;
+use Checkitsedo\Checkitcalendarize\Utility\ConfigurationUtility;
 use TYPO3\CMS\Core\Category\CategoryRegistry;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -11,5 +11,5 @@ if (!(bool)ConfigurationUtility::get('disableDefaultEvent')) {
     Register::extTables(Register::getDefaultCalendarizeConfiguration());
 
     $categoryRegistry = GeneralUtility::makeInstance(CategoryRegistry::class);
-    $categoryRegistry->add('calendarize', 'tx_calendarize_domain_model_event');
+    $categoryRegistry->add('checkitcalendarize', 'tx_checkitcalendarize_domain_model_event');
 }
