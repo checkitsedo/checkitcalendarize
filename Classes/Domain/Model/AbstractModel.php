3bf6c060632af39a163cd45f14fa2523dcef1db8
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 16:04:17 +0200

    Update AbstractModel.php

--- Classes/Domain/Model/AbstractModel.php
+++ Classes/Domain/Model/AbstractModel.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\Domain\Model;
+namespace Checkitsedo\Checkitcalendarize\Domain\Model;
 
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
