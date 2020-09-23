32ea6482a4b733a31e4fb159e40a4634e07036e4
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 19:54:19 +0200

    Update and rename tx_calendarize_domain_model_event.php to tx_checkitcalendarize_domain_model_event.php

--- Configuration/TCA/tx_checkitcalendarize_domain_model_event.php
+++ Configuration/TCA/tx_checkitcalendarize_domain_model_event.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 use HDNET\Autoloader\Utility\ArrayUtility;
 use HDNET\Autoloader\Utility\ModelUtility;
 use HDNET\Autoloader\Utility\TranslateUtility;
-use HDNET\Calendarize\Domain\Model\Event;
-use HDNET\Calendarize\Service\TcaService;
-use HDNET\Calendarize\Utility\ConfigurationUtility;
+use Checkitsedo\Checkitcalendarize\Domain\Model\Event;
+use Checkitsedo\Checkitcalendarize\Service\TcaService;
+use Checkitsedo\Checkitcalendarize\Utility\ConfigurationUtility;
 use TYPO3\CMS\Core\Resource\File;
 
 $base = ModelUtility::getTcaInformation(Event::class);
@@ -96,8 +96,8 @@ $search = [
 ];
 $replace = [
     ',',
-    'language,--div--;' . TranslateUtility::getLllOrHelpMessage('files', 'calendarize') . ',images,downloads,--div--',
-    TranslateUtility::getLllOrHelpMessage('dateOptions', 'calendarize'),
+    'language,--div--;' . TranslateUtility::getLllOrHelpMessage('files', 'checkitcalendarize') . ',images,downloads,--div--',
+    TranslateUtility::getLllOrHelpMessage('dateOptions', 'checkitcalendarize'),
 ];
 
 $tca['types']['1']['showitem'] = \str_replace($search, $replace, $tca['types']['1']['showitem']);
