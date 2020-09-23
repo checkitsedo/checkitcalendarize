e3e5f80770d6f4ac3c813e784249a19853a50c3d
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 19:27:56 +0200

    Update ListViewHelper.php

--- Classes/ViewHelpers/Uri/ListViewHelper.php
+++ Classes/ViewHelpers/Uri/ListViewHelper.php
@@ -5,12 +5,12 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\ViewHelpers\Uri;
+namespace Checkitsedo\Checkitcalendarize\ViewHelpers\Uri;
 
 /**
  * Uri to the list.
  */
-class ListViewHelper extends \HDNET\Calendarize\ViewHelpers\Link\ListViewHelper
+class ListViewHelper extends \Checkitsedo\Checkitcalendarize\ViewHelpers\Link\ListViewHelper
 {
     /**
      * Render the uri to the given list.
