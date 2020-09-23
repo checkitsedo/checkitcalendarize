6e667e8ea8b3258a9007c9d80c826910d3092501
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 14:06:54 +0200

    Add files via upload

--- Classes/Listener/CalendarizeField.php
+++ Classes/Listener/CalendarizeField.php
@@ -1,1 +1,32 @@
- 
+<?php
+
+declare(strict_types=1);
+
+namespace HDNET\Calendarize\Listener;
+
+use HDNET\Calendarize\Register;
+use TYPO3\CMS\Core\Database\Event\AlterTableDefinitionStatementsEvent;
+
+class CalendarizeField
+{
+    public function __invoke(AlterTableDefinitionStatementsEvent $event): void
+    {
+        $event->addSqlData($this->getCalendarizeDatabaseString());
+    }
+
+    /**
+     * Get the calendarize string for the registered tables.
+     *
+     * @return string
+     */
+    protected function getCalendarizeDatabaseString()
+    {
+        $sql = [];
+        foreach (Register::getRegister() as $configuration) {
+            $fieldName = isset($configuration['fieldName']) ? $configuration['fieldName'] : 'calendarize';
+            $sql[] = 'CREATE TABLE ' . $configuration['tableName'] . ' (' . $fieldName . ' tinytext);';
+        }
+
+        return \implode(LF, $sql);
+    }
+}
