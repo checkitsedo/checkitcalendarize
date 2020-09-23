e81385982d7512117b01421e702d6a77649f67ff
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 13:56:26 +0200

    Create Exception.php

---
+++ Classes/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Calendarize logical exception.
+ */
+declare(strict_types=1);
+
+namespace HDNET\Calendarize;
+
+/**
+ * Calendarize logical exception.
+ */
+class Exception extends \Exception
+{
+}
