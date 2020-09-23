4acddd0b7c7b2d7bf082be0f9a5e957114dc183d
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 19:23:55 +0200

    Update YearViewHelper.php

--- Classes/ViewHelpers/Link/YearViewHelper.php
+++ Classes/ViewHelpers/Link/YearViewHelper.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace HDNET\Calendarize\ViewHelpers\Link;
+namespace Checkitsedo\Checkitcalendarize\ViewHelpers\Link;
 
 /**
  * Link to the year.
@@ -36,7 +36,7 @@ class YearViewHelper extends AbstractLinkViewHelper
         }
         $date = $this->arguments['date'];
         $additionalParams = [
-            'tx_calendarize_calendar' => [
+            'tx_checkitcalendarize_calendar' => [
                 'year' => $date->format('Y'),
             ],
         ];
