b142e235ac806f20d0ccc1e85e73c7bd5116ac8f
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2020-09-23 19:58:19 +0200

    Update pages.php

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 $icon = 'apps-pagetree-folder-contains-calendarize';
 
 $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-    0 => 'Calendarize',
-    1 => 'calendarize',
+    0 => 'Checkitcalendarize',
+    1 => 'checkitcalendarize',
     2 => $icon,
 ];
 $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-calendarize'] = $icon;
